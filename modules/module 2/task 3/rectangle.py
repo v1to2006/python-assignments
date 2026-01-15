679c073a6e4180003dbd50d6d3aa75cfcce96052
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-01-15 12:07:21 +0200

    Implemented task 2.3

---
+++ modules/module 2/task 3/rectangle.py
@@ -0,0 +1,8 @@
+rectangleBase = input("Enter base of rectangle: ")
+rectangleHeight = input("Enter height of rectangle: ")
+
+area = float(rectangleBase) * float(rectangleHeight)
+perimiter = 2 * (float(rectangleBase) + float(rectangleHeight))
+
+print("Area: ", area)
+print("Perimeter: ", perimiter)
