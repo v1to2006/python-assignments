5f4c7424f8f9e31071f412c9fb88f848a1516ee9
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-01-16 13:08:31 +0200

    fix exercise 2.3

--- modules/module-2/task-3/rectangle.py
+++ modules/module-2/task-3/rectangle.py
@@ -2,7 +2,7 @@ rectangleBase = input("Enter base of rectangle: ")
 rectangleHeight = input("Enter height of rectangle: ")
 
 area = float(rectangleBase) * float(rectangleHeight)
-perimiter = 2 * (float(rectangleBase) + float(rectangleHeight))
+perimiter = 2 * float(rectangleBase) + 2 * float(rectangleHeight)
 
 print("Area: ", area)
 print("Perimeter: ", perimiter)
