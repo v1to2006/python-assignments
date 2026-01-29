799e02d279916d5757c2c65604d4704091939ffe
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-01-29 11:03:17 +0200

    Implemented exercise 8.2

---
+++ modules/module-8/task-2/main.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+DB_CONFIG = {
+	"host": "127.0.0.1",
+	"port": 3306,
+	"user": "admin",
+	"password": "admin",
+	"database": "flight_game"
+}
+
+def get_airport_amounts(connection, iso_country):
+	cursor = connection.cursor()
+	query = f"SELECT iso_country, type, COUNT(*) AS airport_count FROM flight_game.airport WHERE iso_country=\"{iso_country}\" GROUP BY iso_country, type ORDER BY airport_count DESC, type"
+
+	cursor.execute(query)
+
+	rows = cursor.fetchall()
+	cursor.close()
+	return rows
+
+def print_rows(rows):
+	if rows == []:
+		print("Not found")
+		return
+
+	for i in rows:
+		print(f"{i[1]}: {i[2]}")
+
+def main():
+	connection = mysql.connector.connect(**DB_CONFIG)
+
+	iso_country = input("Enter country code (e.g FI, US): ")
+	rows = get_airport_amounts(connection, iso_country)
+	print_rows(rows)
+
+main()
